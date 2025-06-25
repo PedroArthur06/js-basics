d90d84a56ae2876e3fbfa6466946008dc96bb532
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-06-25 14:39:14 -0400

    add: first exercise with switch case

---
+++ exercises/conditionals/Switch...case.js
@@ -0,0 +1,14 @@
+let role 
+
+switch (role){
+  case 'guest':
+    console.log('Welcome, guest!');
+    break;
+
+  case 'moderator':
+    console.log('Welcome, moderator!');
+    break;
+
+    default:
+      console.log('Welcome, user!');
+}
