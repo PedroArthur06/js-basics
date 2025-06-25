6ec92e666c7813fa3840c007698795f7b4957593
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-06-25 15:55:17 -0400

    Atualiza exemplos de for-in e for-of

--- exercises/Loops/For-in and For-of.js
+++ exercises/Loops/For-in and For-of.js
@@ -5,3 +5,9 @@ for (let key in person) {
   console.log(key,'=', person[key]);
 }
 console.log('=+'.repeat(8));
+
+// for-of loop example
+const numbers = [1, 2, 3, 4, 5];
+for (let number of numbers) {
+  console.log(number);
+}
