f92aeb640e5f132cdc0da4d86fa0e2b4d8cb00c0
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-06-26 10:55:46 -0400

    show which numbers are prime

---
+++ algorithms/Prime Numbers.js
@@ -0,0 +1,18 @@
+function showPrimes(limit){
+  let primes = [];
+  for (let num = 2; num <= limit; num++) {
+    let isPrime = true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      primes.push(num);
+    }
+  }
+  console.log(primes);
+}
+
+showPrimes(100);
