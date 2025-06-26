caa43dc2ab3be35aa95427d27fabdc2f6b9af757
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-06-26 14:29:56 -0400

    displaying data

---
+++ Objects/algorithms/Blog Post Object.js
@@ -0,0 +1,13 @@
+let post = {
+  title: 'a',
+  body: 'b', 
+  author: 'c',
+  views: 10,
+  comments: [
+    { text: 'nice post' },
+    { text: 'great post' },
+  ],
+  isLive: true
+};
+
+console.log(post);
